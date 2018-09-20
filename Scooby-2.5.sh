f82331cdb7a0ef7f64c5dfd1254c727106694fad
Scotty Franzyshen <sfranzyshen@gmail.com>
2018-09-19 18:51:18 -0700

    Update Scooby-2.5.sh

--- Scooby-2.5.sh
+++ Scooby-2.5.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# 9/19/2018 ver 0.0.0 support SARndbox-2.5
+# 9/19/2018 ver 0.0.0 support for SARndbox-2.5, Kinect-3.6, Vrui-4.5-004
+# includes udev usb encoder button support & weather script support
+# wget https://raw.githubusercontent.com/sfranzyshen/ifl/master/Scooby-2.5.sh
+# bash Scooby-2.5.sh
 
 cd ~
 wget http://idav.ucdavis.edu/~okreylos/ResDev/Vrui/Build-Ubuntu.sh
@@ -20,6 +23,9 @@ tar xfz SARndbox-2.5.tar.gz
 cd SARndbox-2.5
 make
 ls ./bin
+
+read -p "Plug in your 3D Camera and Press enter to continue"
+sudo /usr/local/bin/KinectUtil getCalib 0
 
 cd ~
 wget https://github.com/sfranzyshen/ifl/raw/master/SARndbox-2.5_IFL-addons.tar.gz

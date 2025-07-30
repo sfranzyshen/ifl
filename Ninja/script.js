f0690e9334949839e034af1bf9bf6b98faa9d12c
Scotty Franzyshen <sfranzyshen@gmail.com>
2025-07-29 18:51:48 -0700

    Update script.js

--- Ninja/script.js
+++ Ninja/script.js
@@ -689,7 +689,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Register Service Worker
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js')
+            navigator.serviceWorker.register('/ifl/Ninja/service-worker.js')
                 .then((registration) => {
                     console.log('Service Worker registered with scope:', registration.scope);
                 })
